5f9c16fe6bc31049332338002367a38d55b5e563
Alexey <alex1976212@gmail.com>
2024-02-21 17:17:48 +0300

    YP-JavaCourse, 7 спринт-Сетевые запросы

--- src/main/java/technologies/internet/httpClient/Client.java
+++ src/main/java/technologies/internet/httpClient/Client.java
@@ -7,18 +7,23 @@ import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 
 public class Client {
-    public static void main(String[] args) throws IOException, InterruptedException {
-        URI uri = URI.create("https://www.youtube.com/watch?v=XpBRuwK5aN4");
+    public static void main(String[] args) {
+        HttpClient client = HttpClient.newHttpClient();
+
+        URI uri = URI.create("https://api.agify.io/?name=Alexey"); // параметр строки запроса - для доп информации,
+        // видна пользователю
         HttpRequest request = HttpRequest.newBuilder()
                 .GET()
+                .header("Accept", "application/json") // заголовок запроса - для служебной информации
                 .uri(uri)
-                .version(HttpClient.Version.HTTP_1_1)
-                .header("Accept", "text/html")
                 .build();
-        HttpClient client = HttpClient.newHttpClient();
-        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
 
-        System.out.println("Код ответа " + response.statusCode());
-        System.out.println("Тело ответа " + response.body());
+        try {
+            HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+            System.out.println("Status code: " + response.statusCode());
+            System.out.println("Answer in JSON format: " + response.body());
+        } catch (IOException | InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
